436b0932bd85b69804e4c971a3a313a0ace6665a
liderhammer-gif <liderhammer@gmail.com>
2026-09-15 18:56:59 -0300

    Load public contact form styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./admin.css";
+import "./forms.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://croissant.cl"),
