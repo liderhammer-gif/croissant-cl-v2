840fae6e5fe8e205072e899295b5468a78a2dc55
liderhammer-gif <liderhammer@gmail.com>
2026-09-16 19:16:18 -0300

    Protect private routes from search crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://croissant.cl/sitemap.xml"
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/atelier-prive/", "/api/"]
+    },
+    sitemap: "https://croissant.cl/sitemap.xml",
+    host: "https://croissant.cl"
   };
 }
