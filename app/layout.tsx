3ce9fc090dac253439fbc57be9ba7b6e9b9b0ca8
liderhammer-gif <liderhammer@gmail.com>
2026-09-16 19:16:53 -0300

    Fix typed Googlebot metadata directives

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,9 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-      "max-video-preview": -1
+      maxImagePreview: "large",
+      maxSnippet: -1,
+      maxVideoPreview: -1
     }
   },
   openGraph: {
